dc0f2547da23e67c009bd66993c73939c32cf258
Adam Hale <adam.hale92@googlemail.com>
2024-02-27 09:29:27 +0000

    updated button labels. todo: update tests

--- lendlogic-app/app/cookies/CookieButton.jsx
+++ lendlogic-app/app/cookies/CookieButton.jsx
@@ -9,12 +9,13 @@ export function CookieButton({ setCookie, user }) {
   };
 
   return (
-    <button data-testid="test-cookie"
+    <button
+      data-testid="test-cookie"
       type="button"
       className="mx-4 my-6 min-w-button-width w-26 bg-off-white rounded-3xl text-purple-accent p-3 shadow-card text-center text-2xl hover:bg-purple-accent hover:text-off-white hover:font-semibold"
       onClick={setAndPush}
     >
-      {user}'s journey
+      Login as {user}
     </button>
   );
 }
